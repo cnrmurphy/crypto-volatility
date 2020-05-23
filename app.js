883a21a910a70c1cc06ebfae6b5bcecfc1ecf8e7
cjm322 <conor.murphy@gmail.com>
2020-05-23 02:22:59 -0400

    removing folders that shouldn't be here

--- app.js
+++ app.js
@@ -3,5 +3,18 @@ const MessariClient = require('./Messari.js');
 (async () => {
   const mc = new MessariClient('ethereum');
   const data = await mc.getPriceSeries();
-  console.log(data);
+  const closingPrices = data.closingPrices;
+  
+  let low = closingPrices[0];
+  let high = closingPrices[0];
+
+  closingPrices.forEach(price => {
+    if (price > high)
+      high = price;
+    if (price < low)
+      low = price;
+  });
+
+  console.log({ low, high });
+  console.log('vol: ', (high-low)/high);
 })();
