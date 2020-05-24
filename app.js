a716728b957a2fdbd951da45fae26528cdc66dd4
cjm322 <conor.murphy@gmail.com>
2020-05-24 02:59:42 -0400

    added percent change

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const math = require('mathjs');
   const mean = closingPrices.reduce((a, b) => a + b, 0) / closingPrices.length;
 
   const mv = math.std(closingPrices);
-  
+
   console.log({
     'asset': data.name,
     currentPrice,
@@ -19,15 +19,18 @@ const math = require('mathjs');
       value: mv,
       '1sd': {
         plus: mean + mv,
-        minus: mean - mv
+        minus: mean - mv,
+        percent: (mv / mean) * 100
       },
       '2sd': {
         plus: mean + (mv * 2),
-        minus: mean - (mv * 2)
+        minus: mean - (mv * 2),
+        percent: ((mv * 2) / mean) * 100
       },
       '3sd': {
         plus: mean + (mv * 3),
-        minus: mean - (mv * 3)
+        minus: mean - (mv * 3),
+        percent: ((mv * 3) / mean) * 100
       }
     },
   });
